29616b101b88962a922878b9f7e7b8b770671f91
Derek Longmuir <derek.longmuir@gmail.com>
2015-07-19 23:12:29 -0400

    Capture coverage summary and save into git notes

--- go.sh
+++ go.sh
@@ -5,10 +5,12 @@ brew install xctool gcovr
 brew cask install oclint
 
 xctool -workspace DutyTime.xcworkspace -scheme DutyTime -sdk iphonesimulator -configuration Debug -IDECustomDerivedDataLocation="build/build_ccov" \
-  -reporter json-compilation-database:compile_commands.json clean test
+  -reporter json-compilation-database:compile_commands.json -reporter pretty -reporter user-notifications clean test
 
-gcovr --object-directory="build/build_ccov/DutyTime/Build/Intermediates/DutyTime.build/""Debug-iphonesimulator/DutyTime.build/Objects-normal/i386/" \
-  --root=. --gcov-exclude='.*(?:DutyTimeTests|Frameworks|ViewController|AppDelegate).*' --print-summary --html --html-details -o build/coverage.html
+exec gcovr --object-directory="build/build_ccov/DutyTime/Build/Intermediates/DutyTime.build/""Debug-iphonesimulator/DutyTime.build/Objects-normal/i386/" \
+  --root=. --gcov-exclude='.*(?:DutyTimeTests|Frameworks|ViewController|AppDelegate).*' --print-summary --html --html-details -o build/coverage.html | tee build/coverage.txt
+
+git notes add -f -F build/coverage.txt
 
 oclint-json-compilation-database -e "Pods*" -- -rc LONG_LINE=150 -report-type html -o build/oclint.html
 mv compile_commands.json build
